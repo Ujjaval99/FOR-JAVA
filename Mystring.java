ac8a4fc30d8da0e63ea205eefd0942bb0ac424ff
PATEL UJJAVAVL <patelbunty800@gmail.com>
2025-08-28 17:14:39 +0530

    Add files via upload

---
+++ Mystring.java
@@ -0,0 +1,13 @@
+public class Mystring {
+    public static void main(String[] args) {
+        String name = "HELLO BUNTY";
+        System.out.println(name);
+
+        int a = 12;
+        float b = 45.256f;
+
+        System.out.printf("THE VALUE OF A IS %d AND THE VALUE OF B IS %f",a,b);
+
+
+    }
+}
